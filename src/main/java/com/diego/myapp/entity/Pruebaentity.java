e9dafa1e580d7c1493122c8be3c151ffaded4497
alessandro pinegro <jpinegro8@gmail.com>
2022-08-10 11:37:54 -0500

    my-aplicación  Version final del proyecto

---
+++ src/main/java/com/diego/myapp/entity/Pruebaentity.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.diego.myapp.entity;
+
+/**
+ * @author jpinegro
+ *
+ */
+public class Pruebaentity {
+	
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+
+}
